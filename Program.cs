7f6401480ec68a83e0281096f8a73b9bc44854bb
Anzhela001 <zakharova3940@gmail.com>
2024-11-16 00:26:01 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿
-using System;
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
 
 class Program
 {
@@ -8,6 +10,16 @@ class Program
         // Получаем строку от пользователя
         Console.Write("Введите строку: ");
         string input = Console.ReadLine();
+
+        // Проверка на наличие неподходящих символов
+        var invalidChars = input.Where(c => !IsValidCharacter(c)).Distinct().ToArray();
+
+        if (invalidChars.Length > 0)
+        {
+            // Если найдены неподходящие символы, выводим сообщение об ошибке
+            Console.WriteLine("Ошибка: строка содержит неподходящие символы: " + string.Join(", ", invalidChars));
+            return;
+        }
 
         string result;
 
@@ -39,6 +51,14 @@ class Program
 
         // Выводим результат
         Console.WriteLine("Обработанная строка: " + result);
+
+        // Выводим информацию о повторениях символов
+        var characterCounts = CountCharacterOccurrences(result);
+        Console.WriteLine("Количество повторений каждого символа:");
+        foreach (var entry in characterCounts)
+        {
+            Console.WriteLine($"'{entry.Key}': {entry.Value}");
+        }
     }
 
     // Метод для переворачивания строки
@@ -47,5 +67,29 @@ class Program
         char[] charArray = str.ToCharArray();
         Array.Reverse(charArray);
         return new string(charArray);
+    }
+
+    // Метод для проверки допустимости символа
+    static bool IsValidCharacter(char c)
+    {
+        return c >= 'a' && c <= 'z'; // Проверка, что символ - это буква от 'a' до 'z'
+    }
+
+    // Метод для подсчёта количества повторений каждого символа
+    static Dictionary<char, int> CountCharacterOccurrences(string str)
+    {
+        var characterCounts = new Dictionary<char, int>();
+        foreach (char c in str)
+        {
+            if (characterCounts.ContainsKey(c))
+            {
+                characterCounts[c]++;
+            }
+            else
+            {
+                characterCounts[c] = 1;
+            }
+        }
+        return characterCounts;
     }
 }
