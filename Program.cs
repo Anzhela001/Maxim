c58097ecb9555ea567a1111161c11fa438813f9e
Anzhela001 <zakharova3940@gmail.com>
2024-11-15 23:02:43 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-﻿
-using System;
+﻿using System;
+using System.Text;
+using System.Linq;
 
 class Program
 {
@@ -8,6 +9,16 @@ class Program
         // Получаем строку от пользователя
         Console.Write("Введите строку: ");
         string input = Console.ReadLine();
+
+        // Проверка на наличие неподходящих символов
+        var invalidChars = input.Where(c => !IsValidCharacter(c)).Distinct().ToArray();
+
+        if (invalidChars.Length > 0)
+        {
+            // Если найдены неподходящие символы, выводим сообщение об ошибке
+            Console.WriteLine("Ошибка: строка содержит неподходящие символы: " + string.Join(", ", invalidChars));
+            return;
+        }
 
         string result;
 
@@ -47,5 +58,11 @@ class Program
         char[] charArray = str.ToCharArray();
         Array.Reverse(charArray);
         return new string(charArray);
+    }
+
+    // Метод для проверки допустимости символа
+    static bool IsValidCharacter(char c)
+    {
+        return c >= 'a' && c <= 'z'; // Проверка, что символ - это буква от 'a' до 'z'
     }
 }
